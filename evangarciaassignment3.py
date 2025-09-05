1003a8cd3b26d64238ca85f176aec8f0f27446c4
evun <EvanGarcia535@gmail.com>
2025-09-05 12:09:23 -0400

    Add files via upload
    added function trainCar that creates train cars with the filledRectangle function. Utilizies "t" the turtle and other turtle module functions.

--- evangarciaassignment3.py
+++ evangarciaassignment3.py
@@ -7,17 +7,12 @@ screen = turtle.Screen()
 
 # function filledSquare creates a filledSquare using "t" the turtle.
 def filledSquare(t, w, color):
-	
 	t.fillcolor(color)
 	t.begin_fill()
 	for i in range(4):
 		t.forward(w)
 		t.right(90)
 	t.end_fill()
-	
-t.up()
-t.goto(-200,0)
-t.down()
 
 # function filledRectangle creates a filledRectangle using "t" the turtle.
 def filledRectangle(t, w, h, color):
@@ -29,11 +24,45 @@ def filledRectangle(t, w, h, color):
 		t.forward(h)
 		t.right(90)
 	t.end_fill()
-	
-def main():
-    #filledSquare(t, 40, "red")
-    filledRectangle(t, 100, 50, "red")
 
+# function trainCar simulates a train car utilzing the function filledRectangle, "t" the turtle, and other turtle functions in the turtle module.
+def trainCar(t, x, y, color):
+	wheelY = y - 80 - 15
+	t.up()
+	t.goto(x, y)
+	t.down()
+	t.fillcolor(color)
+	t.begin_fill()
+	for i in range(2):
+		t.forward(150)
+		t.right(90)
+		t.forward(80)
+		t.right(90)
+
+	# the left wheel for the train car.
+	t.end_fill()
+	t.up()
+	t.goto(x + 15, wheelY)
+	t.down()
+	t.fillcolor("black")
+	t.begin_fill()
+	t.circle(15)
+	t.end_fill()
+
+	# the right wheel for the train car.
+	t.up()
+	t.goto(x + 150 - 15, wheelY)
+	t.down()
+	t.fillcolor("black")
+	t.begin_fill()
+	t.circle(15)
+	t.end_fill()	
+
+# main function will run the application.
+def main():
+	trainCar(t, 150, 0, "yellow")
+	trainCar(t, -50, 0, "green")
+	trainCar(t, -250, 0, "blue")
 main()
 turtle.done()
     
