d04032485859811a2857082ecd8cc3e23a853b28
evun <EvanGarcia535@gmail.com>
2025-09-05 19:08:12 -0400

    the final repo
    created the drawLocomotive function that will create the locomotive for the trains, and then added the ability to write my name onto the locomotive.

--- evangarciaassignment3.py
+++ evangarciaassignment3.py
@@ -25,22 +25,15 @@ def filledRectangle(t, w, h, color):
 		t.right(90)
 	t.end_fill()
 
-# function trainCar simulates a train car utilzing the function filledRectangle, "t" the turtle, and other turtle functions in the turtle module.
-def trainCar(t, x, y, color):
+# function drawTrainCar simulates a train car utilzing the function filledRectangle, "t" the turtle, and other turtle functions in the turtle module.
+def drawTrainCar(t, x, y, color):
 	wheelY = y - 80 - 15
 	t.up()
 	t.goto(x, y)
 	t.down()
-	t.fillcolor(color)
-	t.begin_fill()
-	for i in range(2):
-		t.forward(150)
-		t.right(90)
-		t.forward(80)
-		t.right(90)
+	filledRectangle(t, 150, 80, color)
 
 	# the left wheel for the train car.
-	t.end_fill()
 	t.up()
 	t.goto(x + 15, wheelY)
 	t.down()
@@ -58,11 +51,65 @@ def trainCar(t, x, y, color):
 	t.circle(15)
 	t.end_fill()	
 
-# main function will run the application.
+# function  drawLocomotive simulates a locomotive utilzing the functions filledRectangle and filledSquare, "t" the turtle, and other turtle functions in the turtle module.
+def drawLocomotive(t, x, y):
+	wheelY = y - 80 - 15
+	t.up()
+	t.goto(x, y)
+	t.down()
+	t.fillcolor("red")
+	t.begin_fill()
+	for i in range(2):
+		t.forward(150)
+		t.right(90)
+		t.forward(80)
+		t.right(90)
+	t.end_fill()
+
+	# creates the smoke stack for the locomotive.
+	t.up()
+	t.goto(x + 20, y + 30)
+	t.down()
+	filledRectangle(t, 20, 60, "gray")
+
+	# creates the area where the driver of the locomotive resides.
+	t.up()
+	t.goto(x + 80, y + 30)
+	t.down()
+	filledSquare(t, 50, "red")
+
+	# the left wheel for the train car.
+	t.up()
+	t.goto(x + 15, wheelY)
+	t.down()
+	t.fillcolor("black")
+	t.begin_fill()
+	t.circle(15)
+	t.end_fill()
+
+	# the right wheel for the train car.
+	t.up()
+	t.goto(x + 150 - 15, wheelY)
+	t.down()
+	t.fillcolor("black")
+	t.begin_fill()
+	t.circle(15)
+	t.end_fill()
+
+	# divides the x coordinate by 2 so "t" the turtle can be centered properly, then subtracts the y coordinate to align the text "Evan Garcia" properly in the locomotive.
+	t.up()
+	t.goto(x + 150/2, y - 60)
+	t.down()
+	t.color("black")
+	t.write("Evan Garcia", align="center", font=("Arial", 16, "bold"))
+	t.color("black")        
+
+# main function will run the application and create our trains!
 def main():
-	trainCar(t, 150, 0, "yellow")
-	trainCar(t, -50, 0, "green")
-	trainCar(t, -250, 0, "blue")
+	drawLocomotive(t, -450, 0)
+	drawTrainCar(t, 150, 0, "yellow")
+	drawTrainCar(t, -50, 0, "green")
+	drawTrainCar(t, -250, 0, "blue")
 main()
 turtle.done()
     
